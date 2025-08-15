77d70f883de18eb1a041b13769f7bda415978931
erikbernard <erikbrsilva@gmail.com>
2025-08-15 10:45:53 -0300

    update: addition of the task table in database

--- src/config/database.ts
+++ src/config/database.ts
@@ -8,6 +8,7 @@ export async function initializeDatabase() {
       driver: sqlite3.Database,
     });
 
+    await db.exec("PRAGMA foreign_keys = ON;"); // Habilita foreign keys no sqlite
     await db.exec(`
       CREATE TABLE IF NOT EXISTS users (
         id TEXT PRIMARY KEY,
@@ -15,6 +16,20 @@ export async function initializeDatabase() {
         email TEXT NOT NULL UNIQUE,
         password TEXT NOT NULL,
         created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+      )
+    `);
+
+    await db.exec(`
+      CREATE TABLE IF NOT EXISTS tasks (
+        id TEXT PRIMARY KEY,
+        title TEXT NOT NULL,
+        description TEXT NOT NULL,
+        priority TEXT CHECK(priority IN ('HIGH', 'MEDIUM', 'LOW')) NOT NULL,
+        status TEXT CHECK(status IN ('PENDING', 'COMPLETED', 'STARTED', 'BLOCKED', 'NOSTARTED')) NOT NULL,
+        user_id TEXT NOT NULL,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+        updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
       )
     `);
 
