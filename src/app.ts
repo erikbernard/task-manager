0157dfaf7fc200e4bd013a0c344559d5906fbd99
erikbernard <erikbrsilva@gmail.com>
2025-08-15 14:26:27 -0300

    feat: adding new routes in app

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,17 @@ import express from "express";
 import { Database } from "sqlite";
 import { errorHandler } from "./shared/middlewares/error.handler";
 import { configureUserRoutes } from "./modules/users/routes/User.routes";
+import { configureTaskRoutes } from "./modules/task/routes/Task.routes";
 
 export function createApp(db: Database) {
   const app = express();
   app.use(express.json());
 
   const userRoutes = configureUserRoutes(db);
+  const taskRoutes = configureTaskRoutes(db);
+
   app.use("/api", userRoutes);
+  app.use("/api", taskRoutes);
 
   app.use(errorHandler);
 
