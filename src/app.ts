20d514fb96c5160d593a0c4fdce7d05c55dd2c92
erikbernard <erikbrsilva@gmail.com>
2025-08-15 16:33:36 -0300

    feat: adding open api swagger and suport cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from '../swagger.json'; 
+import cors from "cors"; 
 import { Database } from "sqlite";
 import { errorHandler } from "./shared/middlewares/error.handler";
 import { configureUserRoutes } from "./modules/users/routes/User.routes";
@@ -6,8 +9,10 @@ import { configureTaskRoutes } from "./modules/task/routes/Task.routes";
 
 export function createApp(db: Database) {
   const app = express();
+  app.use(cors()); 
   app.use(express.json());
 
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   const userRoutes = configureUserRoutes(db);
   const taskRoutes = configureTaskRoutes(db);
 
